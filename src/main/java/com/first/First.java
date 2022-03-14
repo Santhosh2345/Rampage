93f5d23ec318d59af7eba948b5e30cc1a1bf2c35
Santhosh2345 <sk3505712@gmail.com>
2022-03-14 11:23:18 +0530

    F

---
+++ src/main/java/com/first/First.java
@@ -0,0 +1,11 @@
+package com.first;
+
+public class First {
+public static void main(String[] args) {
+	System.setProperty("webdriver.chrome.driver", "C:\\Users\\P.Santhosh Kumar\\eclipse-workspace\\Maven\\Chrome\\chromedriver.exe");
+	WebDriver a = new ChromeDriver();
+	System.out.println("I am");
+	
+	
+}
+}
---
+++ src/test/java/com/first/FirstTest.java
@@ -0,0 +1,18 @@
+package com.first;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class FirstTest {
+	
+public static void main(String[] args) {
+
+	//System.setProperty("webdriver.chrome.driver", "C:\\Users\\P.Santhosh Kumar\\eclipse-workspace\\Maven\\Chrome\\chromedriver.exe");
+     Webdriver a = new ChromeDriver();
+}	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
